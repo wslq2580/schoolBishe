f3eb88b56eed56f0fce4b7c9ffcc69bb8ddd77dc
JB Chen <3399718467@qq.com>
2023-04-12 21:54:37 +0800

    version 0.6

--- jobFinding/jobFinding/salaryView.py
+++ jobFinding/jobFinding/salaryView.py
@@ -1,7 +1,10 @@
 import pymysql
 from pyecharts.charts import Pie
+from pyecharts.charts import Bar
 from pyecharts.faker import Faker
 from pyecharts import options as opts
+from pyecharts.globals import ThemeType
+
 try:
     conn = pymysql.connect(host='localhost', user='root', passwd='123456', port=3306)
     print('连接成功！')
@@ -16,10 +19,30 @@ data = cursor.fetchall()
 dq = []
 dq.extend(data)
 print(dq)
-c = (
+pie = (
     Pie()
-    .add("薪酬统计图", dq)
+    .add("薪酬统计图", dq,rosetype='area')
     .set_global_opts(title_opts=opts.TitleOpts(title="饼图"))
     .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
 )
-c.render("薪酬统计图.html")
+pie.render("薪酬统计图.html")
+
+sql1 = "select requireWorkYears,CEILING(avg(salary_lower)),CEILING(avg(salary_high)) from liepinjob group by requireWorkYears;"
+cursor.execute(sql1)
+data1 = cursor.fetchall()
+dq1 = []
+dq1.extend(data1)
+year = list(zip(*dq1))[0]
+lowSalary = list(zip(*dq1))[1]
+highSalary = list(zip(*dq1))[2]
+print(year,lowSalary,highSalary)
+bar = (
+    Bar(init_opts=opts.InitOpts(theme=ThemeType.LIGHT))
+    .add_xaxis(year)
+    .add_yaxis("最低薪资平均值",lowSalary)
+    .add_yaxis("最高薪资平均值",highSalary)
+    .set_global_opts(title_opts=opts.TitleOpts(title="主标题", subtitle="副标题"))
+    .set_series_opts(markline_opts=["average"])
+)
+
+bar.render("薪酬条形图.html")
